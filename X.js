e63e4c2f16f3125120578030c47519aa1d154f57
trtrtr6 <2450260012@qq.com>
2016-07-19 16:50:41 +0800

    添加一些基本的方法
    添加一些基本的方法

--- X.js
+++ X.js
@@ -8,6 +8,8 @@
   } else {
     factory(window['X'] = {
       Version:'0.0.1',
+      IE : !!(window.attachEvent && !window.opera), //判断ie
+      Webkit :  navigator.userAgent.toLowerCase().indexOf(' applewebkit/') > -1,  //判断webkit内核
       StringUtils:{
         //是否为字符串
         isString: function(obj){
@@ -28,7 +30,7 @@
           if(this.isNotEmpty(str)){
             return str.replace(/\s*/g, '');
           }
-        }
+        },
         //截取指定字符之间的字符串/截取指定字符串字符下标之间的字符串(当只指定一个下标/字符的时候可指所要截取的字符串长度)
         subString : function(begin,end,length){
 
@@ -45,16 +47,16 @@
         getMonth : function(datetime){
           var time = this.getDateTime(datetiem);
           return time.getMonth() + 1;
-        }
+        },
         //获取当前日期的星期(datetime传值时，获取与之对应时间参数)
         getWeek : function(datetime){
           return this.weekArr[this.getWeekIndex(datetime)-1];
-        }
+        },
         //获取当前日期是当月的第几天(datetime传值时，获取与之对应时间参数)
         getDate : function(datetime){
           var time = this.getDateTime(datetiem);
           return time.getDate();
-        }
+        },
         //获取当前日期的小时(datetime传值时，获取与之对应时间参数)
         getHours : function(datetime,is_12){
           var time = this.getDateTime(datetiem);
@@ -63,27 +65,27 @@
             return hours%12 == 0 ? 12 : hours%12;
           }
           return hours;
-        }
+        },
         //获取当前日期的分钟数(datetime传值时，获取与之对应时间参数)
         getMinutes : function(datetime){
           var time = this.getDateTime(datetiem);
           return time.getMinutes();
-        }
+        },
         //获取当前日期的秒数(datetime传值时，获取与之对应时间参数)
         getSeconds : function(datetime){
           var time = this.getDateTime(datetiem);
           return time.getSeconds();
-        }
+        },
         //获取当前日期的毫秒数
         getMilliseconds : function(datetime){
           var time = this.getDateTime(datetiem);
           return time.getMilliseconds();
-        }
+        },
         //获取当前日期是该星期的第几天(datetime传值时，获取与之对应时间参数)
         getWeekIndex : function(datetime){
           var time = this.getDateTime(datetiem);
           return time.getday()+1;
-        }
+        },
         //获取当前日期(通过相关格式)(datetime传值时，获取与之对应时间参数)
         //format相关参数值 "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss"
         getDateFormat : function(datetime,format){
@@ -111,7 +113,7 @@
             }
           }
           return fmt;
-        }
+        },
         //获取当前日期的时间戳(datetime传值时，获取与之对应时间参数)
         getTimestamp : function(datetime,isAddMillisecond){
           var time = this.getDateTime(datetiem);
@@ -120,17 +122,73 @@
             return timeStamp;
           }
           return timeStamp/1000;
-        }
+        },
         //获取当前日期(datetime传值时，获取与之对应时间参数)
         getDateTime : function(datetiem){
           return datetime ? datetime : new Date();
         }
+      },
+
+      //页面加载完成的可执行入口
+      ready : function(fn){
+        if (!this.IE && !this.Webkit && document.addEventListener)  // addEventListener mozilla
+          return document.addEventListener('DOMContentLoaded', fn, false);
+        if (this.IE) (function(){
+          try {
+            document.documentElement.doScroll('left');
+            fn();
+          }catch (err){
+            setTimeout(arguments.callee, 0);
+          }
+        }
+        )();
+        else if (this.Webkit)
+          var t = setInterval(function(){
+            if (/^(loaded|complete)$/.test(document.readyState))
+            clearInterval(t),fn();
+          },0);
       }
     });
   }
 })(function(XExports){
-  //X的全局定义
+  //X的全局定义,对js一些重要类型的方法封装
   var X = typeof XExports !== 'undefined' ? XExports : {};
+
+  $_fn = function(selectName){
+    this.selectName = selectName,
+    this.obj = this.selectElement()
+  }
+  //定义简单的X选择器
+  $_fn.prototype={
+    selectElement:function(){
+      if(typeof this.selectName == 'object'){
+        return this.selectName;
+      }else if (this.selectName.indexOf('#') != -1) {
+        return document.getElementById(this.selectName.replace('#',''));
+      }else if (this.selectName.indexOf('.') != -1) {
+        return document.getElementsByClassName(this.selectName.replace('.',''));
+      }else{
+        return document.getElementsByTagName(this.selectName);
+      }
+    },
+    getSelectName : function(){
+      return this.selectName;
+    },
+    html : function(val){
+      if(val != undefined || val != null){
+        this.obj[0].innerHTML = val
+      }
+      return this.obj[0].innerHTML;
+    },
+    text : function(val){
+      if(val != undefined || val != null){
+        this.obj[0].innerText = val
+      }
+      return this.obj[0].innerText;
+    }
+  }
+  
+  window['$X'] = function(selectName){return new $_fn(selectName)};
 });
 
 
